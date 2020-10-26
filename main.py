68c3e618026b0548eebeb2eeb8e670afb3f35790
Aaron Wilkowitz <aaronwilkowitz@google.com>
2020-10-26 12:29:02 -0400

    update

--- main.py
+++ main.py
@@ -34,10 +34,6 @@ class PewCapital(PersonalCapital):
         with open(self.__session_file, 'w') as data_file:
             data_file.write(json.dumps(self.get_session()))
 
-# test 2f asdfa s
-# asdlfkjasdlfjkas 
-# asdlfkjasdlfjkas 
-# asdlfkjasdlfjkas 
 os.environ["PEW_EMAIL"] = "aaron.wilkowitz@gmail.com"
 os.environ["PEW_PASSWORD"] = "uncasl55"
 
